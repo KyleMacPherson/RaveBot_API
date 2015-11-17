5ec0720d27ca2edc318073d9207fe1baa0bfae6e
kyle macpherson <kaimac2010@gmail.com>
2015-11-17 16:45:17 +0000

    added songs#random

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+
   def create
     json = JSON.parse(request.body.read)
     song = Song.new(json)
@@ -7,5 +8,14 @@ class SongsController < ApplicationController
     else
       render json: song.errors, status: 400
     end
+  end
+
+  def random
+    songs = Song.all
+    party_songs = []
+    songs.each do |song|
+      party_songs << song if song.party_name == json['party_name']
+    end
+    render json: party_songs.sample
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post 'partys' => 'partys#create'
   post 'songs' => 'songs#create'
+  get 'songs/random' => 'songs#random'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
