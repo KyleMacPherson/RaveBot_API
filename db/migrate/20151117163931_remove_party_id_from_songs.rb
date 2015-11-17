ac53a20ba66ef8b668636f1c062e1fa48936cf43
kyle macpherson <kaimac2010@gmail.com>
2015-11-17 16:39:55 +0000

    removed party_id from songs

---
+++ db/migrate/20151117163931_remove_party_id_from_songs.rb
@@ -0,0 +1,5 @@
+class RemovePartyIdFromSongs < ActiveRecord::Migration
+  def change
+    remove_column :songs, :party_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117163652) do
+ActiveRecord::Schema.define(version: 20151117163931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,11 +28,7 @@ ActiveRecord::Schema.define(version: 20151117163652) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "party_id"
     t.string   "party_name"
   end
 
-  add_index "songs", ["party_id"], name: "index_songs_on_party_id", using: :btree
-
-  add_foreign_key "songs", "parties"
 end
